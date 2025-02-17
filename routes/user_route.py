191d3f6775d82573ed8afa43057101112f8f03b2
dieramirezma <dieramirezma@unal.edu.co>
2025-02-16 22:54:01 -0500

    feat(user): add logic to reset password by email

--- routes/user_route.py
+++ routes/user_route.py
@@ -1,6 +1,8 @@
 # Import resources
-from services.user_service import User
+from services.user_service import User, ResetPasswordRequest, ResetPasswordConfirm
 
 # Add resources to the API
 def add_resources(api):
     api.add_resource(User, "/api/user/<int:id>")
+    api.add_resource(ResetPasswordRequest, "/api/user/reset-password-request")
+    api.add_resource(ResetPasswordConfirm, "/api/user/reset-password")
